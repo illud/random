d670ae3c6322f5352265e007816af045127f17f0
saturnavt <61476419+saturnavt@users.noreply.github.com>
2020-03-05 16:05:28 -0500

    random.go with the code added

---
+++ random.go
@@ -0,0 +1,25 @@
+package random
+
+import (
+  "math/rand"
+  "time"
+)
+
+func randomString(count int) string{
+  var arr = []string{"a","b","c","d","e","f","g","h","i","j","l","m","n","o","p","q","r","s","t","w","x","y","z","A","B","C","D","E","F","G","H","I","J","L","M","N","O","P","Q","R","S","T","W","X","Y","Z"}
+  var randomString string
+
+  rand.Seed(time.Now().UnixNano())
+
+  for x := 0; x < count; x++{
+    randomString += arr[rand.Intn(42 - 0 + 1)]
+  }
+
+  return randomString
+}
+
+func randomNumber(max int, min int) int{
+  rand.Seed(time.Now().UnixNano())
+  
+  return rand.Intn(max - min + 1)
+}
